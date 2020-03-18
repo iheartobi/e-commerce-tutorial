5d5dc26199866fb9026aaa6335619e4324a53962
iheartobi <iobidah9@gmail.com>
2020-03-18 14:42:15 -0400

    begin routes for homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/homepage/homepage.components.jsx'
-import { Route } from 'react-router-dom'
+import Homepage from './pages/homepage/homepage.components.jsx';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-   <div>
-     <Route exact path='/' component={Homepage}/>
-   </div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -11,7 +11,7 @@ class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
@@ -45,8 +45,8 @@ class Directory extends Component {
 
   render() {
     return <div className='directory-menu'>
-       {this.state.sections.map(({title, id, imageUrl}) => {
-         return  <MenuItem key={id} title={title} imageUrl={imageUrl}/>
+       {this.state.sections.map(({id, ...sectionProps}) => {
+         return  <MenuItem key={id} {...sectionProps}/>
        })}
     </div>;
   }
