8c11e66d6454f635df606d20476ec736f6f7d658
iheartobi <iobidah9@gmail.com>
2020-03-19 11:46:45 -0400

    added preview items and collection item components plus styling

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.components.jsx';
 import { Route, Switch } from 'react-router-dom';
-
-const HatsPage = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+import ShopPage from './pages/shop/shop.component.jsx'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
