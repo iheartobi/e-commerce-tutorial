817af72287f9e7dd41d749b9e1dcc8bae990bf8e
iheartobi <iobidah9@gmail.com>
2020-03-24 23:41:07 -0400

    finished handle submit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          }, () => console.log(this.state));
+          });
           
         });
       }else{
@@ -37,7 +37,6 @@ class App extends Component {
         })
       }
     });
-    console.log(this.state);
   }
 
   componentWillUnmount() {
